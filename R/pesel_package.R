92167193f2fd8d31e3802ea8718ec511f142ec18
Piotr Sobczyk <sobbombo@gmail.com>
2015-11-04 16:35:04 +0100

    initial commit with functions from varclust

---
+++ R/pesel_package.R
@@ -0,0 +1,12 @@
+#' PESEL
+#'
+#' @docType package
+#' @name varclust
+#' @details Version: 0.1.0
+#' @author Piotr Sobczyk,
+#'         Julie Josse,
+#'         Malgorzata Bogdan
+#'
+#' Maintainer: Piotr Sobczyk \email{Piotr.Sobczyk@@pwr.edu.pl}
+#'
+NULL
