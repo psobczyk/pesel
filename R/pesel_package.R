cabe09d921d91a1aecbc6b5d4385cbad28ca1998
Piotr Sobczyk <sobbombo@gmail.com>
2015-11-18 14:33:29 +0100

    adding example

--- R/pesel_package.R
+++ R/pesel_package.R
@@ -2,11 +2,18 @@
 #'
 #' @docType package
 #' @name pesel
-#' @details Version: 0.3.0
+#' @details Version: 0.3.1
 #' @author Piotr Sobczyk,
 #'         Julie Josse,
 #'         Malgorzata Bogdan
 #'
 #' Maintainer: Piotr Sobczyk \email{Piotr.Sobczyk@@pwr.edu.pl}
 #'
+#' @examples
+#' \dontrun{
+#' library(varclust)
+#' set.seeed(1)
+#' sim.data <- data.simulation(n = 100, SNR = 1, K=1, numb.vars = 200, max.dim = 5)
+#' pesel(sim.data$X, 1, 10)
+#' }
 NULL
