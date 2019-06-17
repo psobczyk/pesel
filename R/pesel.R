7e866036faccddd979e88652bf2800a076a9faf9
Piotr Sobczyk <Piotr.Sobczyk@pwr.edu.pl>
2019-06-17 20:58:49 +0200

    adding examples

--- R/pesel.R
+++ R/pesel.R
@@ -27,6 +27,22 @@
 #' @export
 #' @return number of components
 #' @examples
+#' # EXAMPLE 1 - noise
+#' with(set.seed(23), pesel(matrix(rnorm(10000), ncol = 100), npc.min = 0))
+#'
+#' # EXAMPLE 2 - fixed effects PCA model
+#' sigma <- 0.5
+#' k <-  5
+#' n <- 100
+#' numb.vars <- 10
+#' # factors are drawn from normal distribution
+#' factors <- replicate(k, rnorm(n, 0, 1))
+#' # coefficients are drawn from uniform distribution
+#' coeff <- replicate(numb.vars, rnorm(k, 0, 1))
+#' SIGNAL <- scale(factors %*% coeff)
+#' X <- SIGNAL + replicate(numb.vars, sigma * rnorm(n))
+#' pesel(X)
+#'
 #' \dontrun{
 #' library(MetabolAnalyze)
 #' data(UrineSpectra)
--- R/pesel_package.R
+++ R/pesel_package.R
@@ -3,7 +3,7 @@
 #' @description Automatic estimation of number of principal components in PCA
 #' with PEnalized SEmi-integrated Likelihood (PESEL).
 #'
-#' @details Version: 0.7.1
+#' @details Version: 0.7.2
 #' @docType package
 #' @name pesel
 #' @importFrom stats cov
@@ -19,6 +19,22 @@
 #' Journal of Computational and Graphical Statistics 2017
 #'
 #' @examples
+#' # EXAMPLE 1 - noise
+#' with(set.seed(23), pesel(matrix(rnorm(10000), ncol = 100), npc.min = 0))
+#'
+#' # EXAMPLE 2 - fixed effects PCA model
+#' sigma <- 0.5
+#' k <-  5
+#' n <- 100
+#' numb.vars <- 10
+#' # factors are drawn from normal distribution
+#' factors <- replicate(k, rnorm(n, 0, 1))
+#' # coefficients are drawn from uniform distribution
+#' coeff <- replicate(numb.vars, rnorm(k, 0, 1))
+#' SIGNAL <- scale(factors %*% coeff)
+#' X <- SIGNAL + replicate(numb.vars, sigma * rnorm(n))
+#' pesel(X)
+#'
 #' \dontrun{
 #' library(varclust)
 #' set.seed(1)
